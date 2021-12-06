e77f10f63bccf6db59b8e72308a8e9853d60a4c5
Andrey Starovoyt <andrey.starovoyt@jetbrains.com>
2021-12-06 20:18:53 +0300

    task 6 part 1

---
+++ task6/part1.ts
@@ -0,0 +1,27 @@
+import {readTextBuffer} from "../util.ts";
+
+const data = readTextBuffer(import.meta.url);
+const numbers = data.split(",").map(Number);
+const days = 80;
+
+let currentArray = numbers;
+for (let _i = 0; _i < days; _i++) {
+    let toAdd = 0;
+    const newArray = [];
+    for (let i = 0; i < currentArray.length; i++){
+        const number = currentArray[i];
+        const isZero = number == 0;
+        const newNumber = isZero ? 6 : number - 1;
+        newArray.push(newNumber);
+        if (isZero) toAdd++;
+    }
+    for (let i = 0; i < toAdd; i++) {
+        newArray.push(8);
+    }
+    currentArray = newArray;
+}
+
+console.log(currentArray.length);
+Deno.exit();
+
+
