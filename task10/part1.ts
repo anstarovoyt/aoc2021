1fec0e524f7121dfe7e1afcebb099076f866ec50
Andrey Starovoyt <andrey.starovoyt@jetbrains.com>
2021-12-10 11:00:59 +0300

    task 10

---
+++ task10/part1.ts
@@ -0,0 +1,39 @@
+import {error, readTextBuffer, split} from "../util.ts";
+import {braces} from "./braces.ts";
+
+const lines = split(readTextBuffer(import.meta.url));
+
+function cost(symbol: string) {
+    switch (symbol) {
+        case ")":
+            return 3;
+        case "]":
+            return 57;
+        case "}":
+            return 1197;
+        case ">":
+            return 25137;
+    }
+    error("Incorrect state");
+}
+
+
+const incorrect: string[] = [];
+
+for (const line of lines) {
+    const expected: string[] = [];
+    for (const lineElement of line) {
+        const close = braces[lineElement];
+        if (close == null) {
+            if (expected.pop() != lineElement) {
+                incorrect.push(lineElement);
+                break;
+            }
+        } else {
+            expected.push(close);
+        }
+    }
+}
+
+console.log(incorrect.reduce((previousValue, currentValue) => previousValue + cost(currentValue), 0));
+Deno.exit();
---
+++ task10/part2.ts
@@ -0,0 +1,44 @@
+import {error, readTextBuffer, split} from "../util.ts";
+import {braces} from "./braces.ts";
+
+const lines = split(readTextBuffer(import.meta.url));
+
+function cost(symbol: string) {
+    switch (symbol) {
+        case ")":
+            return 1;
+        case "]":
+            return 2;
+        case "}":
+            return 3;
+        case ">":
+            return 4;
+    }
+    error("Incorrect state");
+}
+
+const toCorrect: string[][] = [];
+
+outer: for (const line of lines) {
+    const expected: string[] = [];
+    for (const lineElement of line) {
+        const expectedClose = braces[lineElement];
+        if (expectedClose == null) {
+            //close
+            if (expected.pop() != lineElement) {
+                continue outer;
+            }
+        } else {
+            expected.push(expectedClose);
+        }
+    }
+    toCorrect.push(expected);
+}
+
+
+const result = toCorrect
+    .map(el => el.reduceRight((sub, brace) => 5 * sub + cost(brace), 0))
+    .sort((a, b) => a - b);
+
+console.log(result[(result.length - 1) / 2]);
+Deno.exit();
